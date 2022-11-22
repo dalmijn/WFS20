3e8efe3d93659991adf4c7e18fcaa5c278460a69
Brendan <brencodeert@outlook.com>
2022-11-22 15:39:46 +0100

    Updated setup.py in preperation for release

--- setup.py
+++ setup.py
@@ -33,6 +33,12 @@ setup(
 	# Pick your license as you wish (should match "license" above)
 	'License :: OSI Approved :: BSD License',
 
+	# Language
+	'Natural Language :: English',
+
+	# OS
+	'Operating System :: OS Independent',
+
 	# Specify the Python versions you support here. In particular, ensure
 	# that you indicate whether you support Python 2, Python 3 or both.
 	'Programming Language :: Python :: 3',
