7053bb3bcab5bd24caf653096373113032e09eb5
Brendan <brencodeert@outlook.com>
2023-02-20 17:18:31 +0100

    First draft for threading

--- src/wfs20/thread.py
+++ src/wfs20/thread.py
@@ -1,11 +1,47 @@
-from wfs20.reader import DataReader
-from wfs20.request import CreateGetRequest
-
+import atexit
 import os
 import sys
 import queue
+import threading
 import time
-import concurrent.futures
+import weakref
+
+atexit.register(_Destruction)
+
+def _Destruction():
+	pass
+
+class WorkerItem:
+	def __init__(self, oid, fn, *args, **kwargs):
+		self.oid = oid
+		self.fn = fn
+		self.args
+		self.kwargs
+
+def Worker():
+	while True:
+		break
+
+def Manager():
+	while True:
+		break
+	pass
+
+class ThreadPool:
+	def __init__(self):
+		self.
+		self._callQueue = queue.SimpleQueue()
+		pass
+	# Context manager	
+	def __enter__(self):
+		pass
+	def __exit__(self):
+		pass
+
+	def Run(self):
+		pass
+	def Clean(self):
+		pass
 
 def aap():
 	sys.stdout.write(f"non\n")
